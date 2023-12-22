99986102d4abe4d8c83d13c5cf3db36400b82918
potashin <potashin.nikita@gmail.com>
2023-12-22 13:49:25 +0200

    fix: pipeline

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-Sidekiq.configure_server do |config|
-  config.redis = {url: Rails.application.credentials.redis.url.sidekiq}
-end
+unless Rails.env.test?
+  Sidekiq.configure_server do |config|
+    config.redis = {url: Rails.application.credentials.redis.url.sidekiq}
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = {url: Rails.application.credentials.redis.url.sidekiq}
+  Sidekiq.configure_client do |config|
+    config.redis = {url: Rails.application.credentials.redis.url.sidekiq}
+  end
 end
